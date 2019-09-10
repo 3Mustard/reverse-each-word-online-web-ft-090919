8ba5694e4fe5dd8a1ea4590dbbb4548e9e858976
Matthew C <m23.cullen@gmail.com>
2019-09-10 17:27:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,6 @@ def reverse_each_word(original_string)
   string_array = original_string.split(" ")
   reversed_array = []
   string_array.each do |word|
-    word_array = word.split
+    word_array = word.split("")
+    
 end
