bd0b08d8b16738d5bae99047cf8413343bffa673
Matthew C <m23.cullen@gmail.com>
2019-09-10 17:36:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(original_string)
-  string_array = original_string.split(" ")
-  string_array.collect do |word|
-    word.split("").reverse.join
+  reverse = original_string.split().collect do |word|
+    word.reverse
   end
-  string_array.join(" ")
+  reverse.join(" ")
 end
