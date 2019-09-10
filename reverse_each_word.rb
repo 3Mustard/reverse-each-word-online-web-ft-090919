f02b72765136d20afc671a2b008ee25192015736
Matthew C <m23.cullen@gmail.com>
2019-09-10 17:33:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(original_string)
   string_array = original_string.split(" ")
-  reversed_array = []
-  string_array.each do |word|
-    reversed_array << word.split("").reverse.join
+  string_array.collect do |word|
+    word.split("").reverse.join
   end
-  reversed_array.join(" ")
+  string_array.join(" ")
 end
